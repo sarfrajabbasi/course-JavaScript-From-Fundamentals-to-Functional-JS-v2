2bc6f587033c5e24dda97bb4d5d2f9430df65c77
sarfrajabbasi <sarfrajk96811@gmail.com>
2022-01-06 23:36:32 +0530

    looping -exercise

---
+++ 15-looping-exercise.js
@@ -0,0 +1,24 @@
+//List trasformatimport 
+
+const game = {
+  'suspects':[
+    {
+      name:"Rusty",
+      color:"orange"
+    },{
+      name:"Miss Scarlet",
+      color:"red"
+    }
+  ]
+}
+function foo() {
+  for(let i = 0;i<game.suspects.length;i++){
+    console.log(game.suspects[i]);
+  }
+}
+// console.log(foo);
+
+for(let pickly in obj){
+  obj[pickly]
+}
+
