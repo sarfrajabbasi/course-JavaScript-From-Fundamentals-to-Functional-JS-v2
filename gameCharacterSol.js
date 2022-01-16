80c91fb949df00c7d762e959a05194a5236ce364
sarfrajabbasi <sarfrajk96811@gmail.com>
2022-01-16 22:42:17 +0530

    create an object with dot and bracket notation

---
+++ gameCharacterSol.js
@@ -0,0 +1,17 @@
+//create an obajct with dot and bracket notation and  that repersent the characters and related data you may find  in a game of clue
+
+var game = {};
+game.murderer = "??";
+game['weapons'] = [
+  
+  {type :'laser',location : 'lab'}
+
+{type : 'angry birds', location: 'beanHouse'},
+
+{type : 'monsterDrink',location:'store'}
+];
+
+game.name = [];
+game.name[0]='Miss Scarlet';
+game.name.push('Mr.Green')
+//['Miss Scarlet','Mr.Green']
