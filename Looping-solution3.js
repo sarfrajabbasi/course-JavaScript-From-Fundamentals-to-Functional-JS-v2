0aa535969f948bb08678cbe2e28928aad0849924
sarfrajabbasi <sarfrajk96811@gmail.com>
2022-01-16 22:48:22 +0530

    looping game solution 3

---
+++ Looping-solution3.js
@@ -0,0 +1,19 @@
+var suspects = [
+  {
+    name : "Rusty",
+    color : "orange"
+  
+  },
+  {
+    name:"Miss Scarlet",
+    color:"red"
+  }
+]
+// const firstColor = game.suspects[0].color;
+// const SecondColor = game.suspects[1].color;
+
+//var [color,color2]=[suspects[0].color,suspects[1].color]
+
+// var [{color:firstColor},{color:SecondColor}]=suspects
+
+var [firstObj,SecondColor,] =suspects
