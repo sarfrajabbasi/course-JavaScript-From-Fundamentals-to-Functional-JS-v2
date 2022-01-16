37bdfb319c0f059098418a57e401fa9b1258e649
sarfrajabbasi <sarfrajk96811@gmail.com>
2022-01-16 22:38:36 +0530

    About Destructuring

---
+++ ES6 Destructuring.js
@@ -0,0 +1,17 @@
+var [a,b,c,...d] = [1,2,3,4,5,6,7,8,9,10]
+// console.log(a,b,c,d);
+// var obj ={name:"sarfraj",
+// sname:"abbasi",
+// sarfraj :function () {
+//   console.log("my name is sarfraj")
+// }
+// }
+// const {name,sname,sarfraj}=obj
+// // console.log(obj.sarfraj)
+// sarfraj();
+
+
+
+
+
+
